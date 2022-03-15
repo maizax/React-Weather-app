7e1838a959bcecb13dd9c64dc7b591e35748d419
Giedrius <94111690+maizax@users.noreply.github.com>
2022-03-15 13:53:25 +0200

    1st upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import classes from "./index.module.css";
+import { BiSearch } from "react-icons/bi";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 function App() {
+  const [data, setData] = useState({});
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=0f915262ffca35ed243d6f6498875bf0`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+          setCity("");
+        });
+    }
+  };
+
+  const clearInput = () => {
+    setCity("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <div className={classes.search}>
+        <div className={classes.searchInputs}>
+          <input
+            placeholder="Enter City name..."
+            onChange={(event) => setCity(event.target.value)}
+            value={city}
+            onKeyPress={getWeather}
+            type="text"
+          />
+
+          <div className={classes.searchIcon}>
+            {city.length > 0 ? (
+              <AiFillCloseCircle onClick={clearInput} />
+            ) : (
+              <BiSearch />
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.container}>
+        <div className={classes.top}>
+          <div className={classes.location}>
+            {data.main ? (
+              <p>
+                Today at {data.name}, {data.sys.country}
+              </p>
+            ) : null}
+          </div>
+          {!data.main && (
+            <div className={classes.search}>
+              <p>Please enter city name to get weather information</p>
+            </div>
+          )}
+          <div className={classes.temp}>
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className={classes.description}>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+          <div className={classes.bottom}>
+            <div className={classes.feels}>
+              {data.main ? (
+                <p className={classes.bold}>
+                  {data.main.feels_like.toFixed()}°C
+                </p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className={classes.humidity}>
+              {data.main ? (
+                <p className={classes.bold}>{data.main.humidity}%</p>
+              ) : null}
+              <p>Humidity</p>
+            </div>
+            <div className={classes.wind}>
+              {data.wind ? (
+                <p className={classes.bold}>{data.wind.speed.toFixed()} KMH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
